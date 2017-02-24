29e8fdb5a86a63daf8bff0790aafb1a12994d94e
Craig Ringer <craig@2ndquadrant.com>
2017-02-24 12:14:22 +0800

    Introduce a test showing synchronous replication of BDR node pairs

--- t/030_syncrep.pl
+++ t/030_syncrep.pl
@@ -4,7 +4,7 @@ use Cwd;
 use Config;
 use PostgresNode;
 use TestLib;
-use Test::More tests => 3;
+use Test::More tests => 11;
 require "t/utils/common.pl";
 
 #
@@ -51,3 +51,66 @@ node_c:send
 node_d:apply
 node_d:send],
 '4-node application_name check');
+
+# Everything working?
+$node_a->safe_psql('bdr_test', q[SELECT bdr.bdr_replicate_ddl_command($DDL$CREATE TABLE public.t(x text)$DDL$)]);
+# Make sure everything caught up by forcing another lock
+$node_a->safe_psql('bdr_test', q[SELECT bdr.acquire_global_lock('write')]);
+
+my @nodes = ($node_a, $node_b, $node_c, $node_d);
+for my $node (@nodes) {
+  $node->safe_psql('bdr_test', q[INSERT INTO t(x) VALUES (bdr.bdr_get_local_node_name())]);
+}
+$node_a->safe_psql('bdr_test', q[SELECT bdr.acquire_global_lock('write')]);
+
+# With a node down we should still be able to do work
+$node_b->stop;
+$node_a->safe_psql('bdr_test', q[INSERT INTO t(x) VALUES ('node_a - async')]);
+$node_b->start;
+
+diag "reconfiguring into synchronous pairs A<=>B, C<=>D";
+$node_a->safe_psql('bdr_test', q[ALTER SYSTEM SET synchronous_standby_names = '"node_b:send"']);
+$node_b->safe_psql('bdr_test', q[ALTER SYSTEM SET synchronous_standby_names = '"node_a:send"']);
+
+$node_c->safe_psql('bdr_test', q[ALTER SYSTEM SET synchronous_standby_names = '"node_d:send"']);
+$node_d->safe_psql('bdr_test', q[ALTER SYSTEM SET synchronous_standby_names = '"node_c:send"']);
+
+for my $node (@nodes) {
+  $node->restart;
+}
+
+# Everything should work while the system is all-up
+$node_a->safe_psql('bdr_test', q[INSERT INTO t(x) VALUES ('node_a - sync B-up')]);
+
+# but with node B down, node A should refuse to confirm commit
+diag "stopping B";
+$node_b->stop;
+my $timed_out;
+diag "inserting on A when B is down; expect psql timeout in 10s";
+$node_a->psql('bdr_test', q[INSERT INTO t(x) VALUES ('node_a - sync B-down')], timeout => 10, timed_out => \$timed_out);
+ok($timed_out, 'txn on A times out if B is down');
+
+is($node_a->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_a - sync B-down']), '', 'committed xact not visible on A yet');
+
+is($node_c->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_a - sync B-down']), '1', 'committed xact visible on C');
+
+# but commiting on C should become immediately visible on both A and D when B is down
+# TODO: wait for sync-up better
+$node_c->safe_psql('bdr_test', q[INSERT INTO t(x) VALUES ('node_c - sync B-down')]);
+sleep(2);
+is($node_c->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_c - sync B-down']), '1', 'C xact visible on C');
+is($node_a->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_c - sync B-down']), '1', 'C xact visible on A');
+is($node_d->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_c - sync B-down']), '1', 'C xact visible on D');
+
+diag "starting B";
+$node_b->start;
+
+# Because Pg commits a txn in sync rep before checking sync, once B comes back up
+# and catches up, we should see the txn from when it was down.
+#
+# FIXME: use slot catchup
+
+sleep(2);
+is($node_b->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_a - sync B-down']), '1', 'B received xact from A');
+
+is($node_a->safe_psql('bdr_test', q[SELECT 1 FROM t WHERE x = 'node_a - sync B-down']), '', 'committed xact visible on A after B confirms');
