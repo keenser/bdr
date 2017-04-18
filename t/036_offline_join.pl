ebf92b35ac159cdf8c44fc8c083de19f702b3333
Craig Ringer <craig@2ndquadrant.com>
2017-04-18 14:55:31 +0800

    Note that we should test partitioned nodes during join
    Right now the offline join test stops the offline node. But what if it's just
    unreachable and still processing txns of its own? We need to fix that and
    ensure that the join captures all changes.

--- t/036_offline_join.pl
+++ t/036_offline_join.pl
@@ -29,8 +29,17 @@ my $offline_node = $nodes->[$offline_index];
 my $node_2 = $nodes->[2];
 my @online_nodes = ($node_0, $node_2);
 
+$node_0->safe_psql($bdr_test_dbname, q[CREATE TABLE testinsert(x integer primary key);]);
+wait_for_apply($node_0, $node_1);
+wait_for_apply($node_0, $node_2);
+
 # bring down a node and try to do a node join of a new node.
 $offline_node->stop;
+
+# TODO: do some work on the offline node, verify that it replicates to the
+# joining node. We'll need a better way to bring it offline than shutting it
+# down, especially since we must suppress outbound connections as well as
+# inbound.
 
 my $new_node_name = 'new_logical_join_node';
 my $new_node = get_new_node($new_node_name);
@@ -39,6 +48,8 @@ my $join_query = generate_bdr_logical_join_query($new_node, $node_0);
 # The join query will always complete immediately, we need to look at the
 # progress of join completion to determine whether it gets anywhere.
 $new_node->safe_psql($bdr_test_dbname, $join_query);
+
+# TODO: do some more work on offline node here
 
 # We should never become ready since we'll be stuck at catchup
 $new_node->psql($bdr_test_dbname, "SELECT bdr.bdr_node_join_wait_for_ready()",
@@ -63,11 +74,15 @@ push @online_nodes, $new_node;
 # Everything is happy?
 $new_node->safe_psql($bdr_test_dbname, q[SELECT bdr.acquire_global_lock('ddl')]);
 
+# TODO: check offline work is sync'd
+
 
 
 # A physical join should run bdr_init_copy fine when a node is offline, then get
 # stuck in catchup mode, just like logical join.
 $offline_node->stop;
+
+# TODO: do some work on offline node here
 
 my $new_physical_join_node = get_new_node('new_physical_join_node');
 my $new_conf_file = copy_transform_postgresqlconf( $new_physical_join_node, $node_0 );
@@ -83,6 +98,8 @@ is($timedout, 1, 'Physical node join timed out while node down');
 is($new_physical_join_node->safe_psql($bdr_test_dbname, "SELECT node_status FROM bdr.bdr_nodes WHERE node_name = '" . $new_physical_join_node->name . "'"), 'c',
     "bdr.bdr_nodes still in status 'c'");
 
+# TODO: do more work on offline node here
+
 # If we bring up the new node, join should proceed?
 $offline_node->start;
 
@@ -92,5 +109,7 @@ is($new_physical_join_node->psql($bdr_test_dbname, "SELECT bdr.bdr_node_join_wai
 foreach my $node (@{$nodes}) {
     check_join_status($new_physical_join_node, $node);
 }
+
+# TODO: check offline work is sync'd
 
 done_testing();
