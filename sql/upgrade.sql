1782b928327c77b6da0380b8371a54c2f29bc778
Craig Ringer <craig@2ndquadrant.com>
2017-06-23 14:50:28 +0800

    Release 2.0.1

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -19,6 +19,8 @@ CREATE EXTENSION bdr VERSION '1.0.3.0';
 DROP EXTENSION bdr;
 CREATE EXTENSION bdr VERSION '2.0.0.0';
 DROP EXTENSION bdr;
+CREATE EXTENSION bdr VERSION '2.0.1.0';
+DROP EXTENSION bdr;
 
 -- evolve version one by one from the oldest to the newest one
 CREATE EXTENSION bdr VERSION '1.0.0.0';
@@ -26,6 +28,7 @@ ALTER EXTENSION bdr UPDATE TO '1.0.1.0';
 ALTER EXTENSION bdr UPDATE TO '1.0.2.0';
 ALTER EXTENSION bdr UPDATE TO '1.0.3.0';
 ALTER EXTENSION bdr UPDATE TO '2.0.0.0';
+ALTER EXTENSION bdr UPDATE TO '2.0.1.0';
 
 -- Should never have to do anything: You missed adding the new version above.
 ALTER EXTENSION bdr UPDATE;
