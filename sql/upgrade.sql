fb54e5a8bc99bb33fee9d83201aa41e1b55c2ca6
David Jung <david.jung@zollsoft.de>
2023-12-04 13:59:35 +0100

    Fixed erroneous deletion of pg_depend entries and bumped version to 2.0.7

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -31,6 +31,8 @@ CREATE EXTENSION bdr VERSION '2.0.5.0';
 DROP EXTENSION bdr;
 CREATE EXTENSION bdr VERSION '2.0.6.0';
 DROP EXTENSION bdr;
+CREATE EXTENSION bdr VERSION '2.0.7.0';
+DROP EXTENSION bdr;
 
 -- evolve version one by one from the oldest to the newest one
 CREATE EXTENSION bdr VERSION '1.0.0.0';
@@ -44,6 +46,7 @@ ALTER EXTENSION bdr UPDATE TO '2.0.3.0';
 ALTER EXTENSION bdr UPDATE TO '2.0.4.0';
 ALTER EXTENSION bdr UPDATE TO '2.0.5.0';
 ALTER EXTENSION bdr UPDATE TO '2.0.6.0';
+ALTER EXTENSION bdr UPDATE TO '2.0.7.0';
 
 -- Should never have to do anything: You missed adding the new version above.
 ALTER EXTENSION bdr UPDATE;
